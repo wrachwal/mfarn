03636a5636b5080966d846234b1a260b1d081d35
Waldemar Rachwał <waldemar.rachwal@gmail.com>
2015-12-09 21:28:27 +0100

    Change deprecated Dict, HashDict, Set, and HashSet.
    Dict         -> Map | Keyword   # thus goodbye "generality" :(
    HashDict     -> Map
    Set, HashSet -> MapSet

--- lib/mix/tasks/check.ex
+++ lib/mix/tasks/check.ex
@@ -12,11 +12,11 @@ defmodule Mix.Tasks.Check do
     is_list(app_file) || throw "not found #{inspect app_res}"
     {:ok, [{:application,^app,app_terms}]} = :file.consult(app_file)
     modules = app_terms[:modules]
-    Enum.reduce modules, {HashSet.new, HashSet.new}, &check_module/2
+    Enum.reduce modules, {MapSet.new, MapSet.new}, &check_module/2
   end
 
   def check_module(module) do
-    check_module(module, {HashSet.new, HashSet.new})
+    check_module(module, {MapSet.new, MapSet.new})
     :ok
   end
 
@@ -32,17 +32,17 @@ defmodule Mix.Tasks.Check do
   end
 
   defp called_remote_funs(tree) do
-    :erl_syntax_lib.fold(&pick_remote_fun/2, HashSet.new, tree) |> Enum.sort
+    :erl_syntax_lib.fold(&pick_remote_fun/2, MapSet.new, tree) |> Enum.sort
   end
 
   defp pick_remote_fun(ast, acc) do
     case ast do
       {:call, line, {:remote, _, {:atom, _, mod}, {:atom, _, fun}}, args} ->
-        acc = Set.put(acc, {line, {mod, fun, length(args)}})
+        acc = MapSet.put(acc, {line, {mod, fun, length(args)}})
         if fun == :make_fun and mod == :erlang do
           case args do
             [{:atom, _, mod}, {:atom, _, fun}, {:integer, _, arity}] ->
-              acc = Set.put(acc, {line, {mod, fun, arity}})
+              acc = MapSet.put(acc, {line, {mod, fun, arity}})
             _ -> nil
           end
         end
@@ -53,18 +53,18 @@ defmodule Mix.Tasks.Check do
 
   defp check_mfa(source, {line, mfa}, {visited, exports}) do
     {mod, fun, arity} = mfa
-    unless Set.member?(exports, mfa) do
-      unless Set.member?(visited, mod) do
-        visited = Set.put(visited, mod)
+    unless MapSet.member?(exports, mfa) do
+      unless MapSet.member?(visited, mod) do
+        visited = MapSet.put(visited, mod)
         fa_list = try do
           # IO.puts "? #{mod}"
           mod.module_info[:exports]
         catch
           _, _ -> []
         end
-        exports = Enum.reduce fa_list, exports, fn {f, a}, mfa_set -> Set.put(mfa_set, {mod, f, a}) end
+        exports = Enum.reduce fa_list, exports, fn {f, a}, mfa_set -> MapSet.put(mfa_set, {mod, f, a}) end
       end
-      unless Set.member?(exports, mfa) do
+      unless MapSet.member?(exports, mfa) do
         IO.puts "#{source}:#{line}: warning: unknown #{inspect mod}.#{fun}/#{arity}"
       end
     end
